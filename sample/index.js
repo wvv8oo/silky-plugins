54cdd578be0e07e6fb856c36dbee758fdfcf03e8
wvv8oo <wvv8oo@gmail.com>
2015-02-28 18:12:51 +0800

    小调整

--- sample/index.js
+++ sample/index.js
@@ -7,15 +7,15 @@
 exports.silkyPlugin = true;
 
 //注册silky插件
-exports.registerPlugin = function(silky) {
+exports.registerPlugin = function(silky, options) {
     //注册handlebars的helper，关于handlebars，请参考：http://handlebarsjs.com/
-    silky.registerHandlebarsHelper('customCommand', function(value, options) {
+    silky.registerHandlebarsHelper('customCommand', function(value, done) {
         //直接返回value，什么也不做，你可以根据需要返回具体的数据
         return value
     });
 
     //将要响应路由时hook
-    silky.registerHook('route:willResponse', function(data, options) {
+    silky.registerHook('route:willResponse', function(data, done) {
         //如果是html文件，则在最后面加上一个时间戳
         if (/\.html$/.test(data.request.url)) {
             var extendText = "<!--" + new Date() + "-->";
@@ -27,7 +27,7 @@ exports.registerPlugin = function(silky) {
     silky.registerHook('build:didCompile', {
         //这里声明使用异步，如果async:true，那么必需显式调用done()
         async: true
-    }, function(data, options, done) {
+    }, function(data, done) {
         //这里可以做任何你想做的事，比如说合并文件等
         return done(null);
     });
